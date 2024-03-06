d7221b3caf4a149d505c2b0a3d57e06a7745874f
KrishnaVamsi-17 <krishnavamsi1730@gmail.com>
2024-03-06 22:39:53 +0530

    Update login controller

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt');
+const validator = require('validator');
 
 const Schema = mongoose.Schema
 
@@ -17,7 +18,18 @@ const userSchema = new Schema({
 
 //static signup method
 userSchema.statics.signup = async function(email,password){
-   
+
+   //validation
+   if(!email || !password){
+    throw Error('All feilds must be filled');
+   }
+   if(!validator.isEmail(email)){
+    throw Error('Email is not valid')
+   }
+   if(!validator.isStrongPassword(password)){
+    throw Error('Password is not strong enough')
+   }
+
 
   const exits = await this.findOne({email})
   if(exits){
@@ -29,6 +41,28 @@ userSchema.statics.signup = async function(email,password){
 
   const user = await this.create({email,password:hash})
 
+
+  return user;
+
+
+}
+
+//static login method
+
+userSchema.statics.login = async function(email,password){
+  if(!email || !password){
+    throw Error('All feilds must be filled');
+   }
+   const user = await this.findOne({email})
+  if(!user){
+    throw Error('Incorrect email')
+  }
+
+  const match = await bcrypt.compare(password,user.password);
+
+  if(!match){
+    throw Error('Incorrect Password')
+  }
 
   return user;
 
